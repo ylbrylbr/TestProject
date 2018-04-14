2e388175bfcc31324d4a937b736dee8d68d6a459
ylbrylbr <1553990434@qq.com>
2018-04-14 12:09:56 +0800

    Initial commit

---
+++ functions.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = abs(1 + 2j)
+b = np.sqrt(5)
+print(a)
+print(b)
+
+print(0 ** 89)
+
+x = set([1, 2, 3, 4])
+y = set([2, 3])
+difference = x.symmetric_difference(y)
+print(difference)
